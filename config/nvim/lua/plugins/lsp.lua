7348fcc73621ad0f7a1178f5dc99c9c9579ee334
Tommy Breslein <tommy.breslein@protonmail.com>
2024-11-16 18:18:56 +0100

    fix lsp auto-import ++ more colorful bash prompt

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -35,6 +35,23 @@ return {
       })
 
       local lsp_capabilities = vim.lsp.protocol.make_client_capabilities()
+      lsp_capabilities.textDocument.completion.completionItem = {
+        documentationFormat = { "markdown", "plaintext" },
+        snippetSupport = true,
+        preselectSupport = true,
+        insertReplaceSupport = true,
+        labelDetailsSupport = true,
+        deprecatedSupport = true,
+        commitCharactersSupport = true,
+        tagSupport = { valueSet = { 1 } },
+        resolveSupport = {
+          properties = {
+            "documentation",
+            "detail",
+            "additionalTextEdits",
+          },
+        },
+      }
       local lspconfig = require("lspconfig")
       local lsp_servers = {
         "bashls",
