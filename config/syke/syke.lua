c50fc8bd12091da1e48349696c5480429321bf91
tbreslein <tommy.breslein@protonmail.com>
2024-11-19 06:57:27 +0100

    temp

--- config/syke/syke.lua
+++ config/syke/syke.lua
@@ -1,1 +1,7 @@
-return { symlinks = require("symlinks") }
+-- return { symlinks = require("symlinks") }
+return {
+  symlinks = {
+    source = "/Users/tommy/.dotfiles/config/git",
+    target = "/Users/tommy/.config/git",
+  },
+}
