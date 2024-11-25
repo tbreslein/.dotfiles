be89c25a0661a5bd808fbbd15a0e275d4ac13023
tbreslein <tommy.breslein@protonmail.com>
2024-11-25 16:29:36 +0100

    stuff

--- config/syke/syke.lua
+++ config/syke/syke.lua
@@ -1,1 +1,27 @@
-return { symlinks = require("symlinks") }
+return {
+  symlinks = require("symlinks"),
+  repos = {
+    {
+      remote = "git@github.com:tbreslein/aoc_2024.git",
+      path = "/Users/tommy/code/aoc_2024",
+    },
+    {
+      remote = "git@github.com:tbreslein/syke.git",
+      path = "/Users/tommy/code/syke",
+    },
+  },
+  shell = {
+    -- {
+    --   cmd = { "ls", "/Users/tommy" },
+    --   hook = { when = "before", what = "ln" },
+    -- },
+    -- {
+    --   cmd = { "ls", "/Users/tommy/.dotfiles" },
+    --   hook = { when = "after", what = "repos" },
+    -- },
+    -- {
+    --   cmd = { "nvim", "--version" },
+    --   hook = { when = "after", what = "main" },
+    -- },
+  },
+}
